9c70238380011287db4c08a27feaea053a2e8302
Waldemar Neto <waldemarnt@gmail.com>
2016-04-08 18:08:10 -0300

    Adds BASE_URI to server connection

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ import Hapi from 'hapi';
 
 const server = new Hapi.Server();
 const PORT = process.env.PORT || 9090;
+const BASE_URI = process.env.BASE_URI || 'localhost';
 
 server.connection({
-  host: 'localhost',
+  host: BASE_URI,
   port: PORT
 });
 
