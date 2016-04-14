7391cf9bd1bc97f8f95ad73f99a58d9b41bbd495
Waldemar Neto <waldemarnt@gmail.com>
2016-04-14 11:53:46 -0300

    Removes unused equipment schema

--- models/equipment.js
+++ models/equipment.js
@@ -2,22 +2,6 @@ import Joi from 'joi';
 
 const Equipment = () => {};
 
-Equipment.prototype.schema = Joi.object().keys({
-  equipment: Joi.array().items(
-    Joi.object().keys({
-      id: Joi.string().required(),
-      description: Joi.string(),
-      serviceLevel: Joi.number().integer().required(),
-      identificationNumber: Joi.number().integer().required(),
-      manufacturingDate: Joi.date(),
-      links: Joi.object().keys({
-        dealer: Joi.string(),
-        model: Joi.string()
-      })
-    })
-  )
-});
-
 Equipment.parseEquipment = (telemetryEquipment) => {
   return {
     type: 'equipment',
