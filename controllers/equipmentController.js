67000ef5962ef38cc54c66bb73a9b9ef1c02093f
Waldemar Neto <waldemarnt@gmail.com>
2016-04-15 14:18:34 -0300

    Adds response handler

--- controllers/equipmentController.js
+++ controllers/equipmentController.js
@@ -1,63 +1,6 @@
+import CanVariablesFetcher from '../fetcher/canVariablesFetcher';
 import Equipment from '../models/equipment';
-import CanVariablesFetcher from '../fetcher/canVariablesFetcher.js';
-
-const errorHandler = {
-  401: (err) => {
-    return {
-      statusCode: 401,
-      response: {
-        errors: [{
-          status: err.response.statusCode,
-          title: err.response.body
-        }]
-      }
-    };
-  },
-  404: (err) => {
-    const getErrors = (response) => {
-      if (response.headers &&
-          response.headers['content-type'] &&
-          response.headers['content-type'] === 'application/json') {
-        const errors = Object.assign({}, response.body);
-
-        if (errors.errors && errors.errors.length > 0) {
-          delete errors.errors[0].href;
-          delete errors.errors[0].details;
-        }
-
-        return errors;
-      }
-
-      return response.body;
-    };
-
-    const errors = getErrors(err.response);
-
-    return {
-      statusCode: 404,
-      response: errors
-    };
-  },
-  unhandleError: () => {
-    return {
-      statusCode: 500,
-      response: {
-        errors: [{
-          status: 500,
-          title: 'An unhandled error occurred'
-        }]
-      }
-    };
-  }
-};
-
-const responseWithError = (request, reply) => (err) => {
-  const handler = errorHandler[err.response.statusCode] || errorHandler.unhandleError;
-  const handledErrors = handler(err);
-  const response = reply(handledErrors.response);
-  response.statusCode = handledErrors.statusCode;
-  return response;
-};
+import ReponseHandler from '../handlers/responseHandler';
 
 const responseWithSingleEquipment = (request, reply) => (equipments, equipmentInformations) => {
   const parsedEquipment = Equipment.parseEquipment(
@@ -107,7 +50,7 @@ class EquipmentController {
       });
     })
     .spread(responseWithEquipments(request, reply))
-    .catch(responseWithError(request, reply));
+    .catch(ReponseHandler.responseWithError(request, reply));
   }
 
   findById(request, reply) {
@@ -129,7 +72,7 @@ class EquipmentController {
       });
     })
     .spread(responseWithSingleEquipment(request, reply))
-    .catch(responseWithError(request, reply));
+    .catch(ReponseHandler.responseWithError(request, reply));
   }
 }
 
---
+++ handlers/responseHandler.js
@@ -0,0 +1,61 @@
+const ResponseHandler = () => {};
+
+const errorHandler = {
+  401: (err) => {
+    return {
+      statusCode: 401,
+      response: {
+        errors: [{
+          status: err.response.statusCode,
+          title: err.response.body
+        }]
+      }
+    };
+  },
+  404: (err) => {
+    const getErrors = (response) => {
+      if (response.headers &&
+          response.headers['content-type'] &&
+          response.headers['content-type'] === 'application/json') {
+        const errors = Object.assign({}, response.body);
+
+        if (errors.errors && errors.errors.length > 0) {
+          delete errors.errors[0].href;
+          delete errors.errors[0].details;
+        }
+
+        return errors;
+      }
+
+      return response.body;
+    };
+
+    const errors = getErrors(err.response);
+
+    return {
+      statusCode: 404,
+      response: errors
+    };
+  },
+  unhandleError: () => {
+    return {
+      statusCode: 500,
+      response: {
+        errors: [{
+          status: 500,
+          title: 'An unhandled error occurred'
+        }]
+      }
+    };
+  }
+};
+
+ResponseHandler.responseWithError = (request, reply) => (err) => {
+  const handler = errorHandler[err.response.statusCode] || errorHandler.unhandleError;
+  const handledErrors = handler(err);
+  const response = reply(handledErrors.response);
+  response.statusCode = handledErrors.statusCode;
+  return response;
+};
+
+module.exports = ResponseHandler;
