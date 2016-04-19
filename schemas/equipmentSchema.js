65288423ae7315c1f5213e48012db532d9d55b86
Glauco Oliveira <gl4uc0@gmail.com>
2016-04-19 17:30:00 -0300

    Include Swagger documentation for Equipments
    We need to provide Swagger documentation as well as narrative
    documentation around our endpoints.

---
+++ schemas/equipmentSchema.js
@@ -0,0 +1,111 @@
+import Joi from 'joi';
+
+const Equipment = Joi.object().keys({
+  type: Joi.string(),
+  id: Joi.string(),
+  attributes: Joi.object().keys({
+    description: Joi.string(),
+    serviceLevel: Joi.number().integer(),
+    identificationNumber: Joi.string(),
+    manufacturingDate: Joi.date(),
+    trackingPoint: Joi.object().keys({
+      location: Joi.object().keys({
+        coordinates: Joi.array(),
+        type: Joi.string()
+      }),
+      status: Joi.string()
+    }),
+    trackingData: Joi.object().keys({
+      ENGINE_HOURS: Joi.string(),
+      ENGINE_SPEED: Joi.string(),
+      MACHINE_DIRECTION: Joi.string()
+    })
+  }),
+  relationships: Joi.object().keys({
+    dealer: Joi.object().keys({
+      links: Joi.object().keys({
+        self: Joi.string(),
+        related: Joi.string()
+      }),
+      data: Joi.object().keys({
+        type: Joi.string(),
+        id: Joi.string()
+      })
+    }),
+    model: Joi.object().keys({
+      links: Joi.object().keys({
+        self: Joi.string(),
+        related: Joi.string()
+      }),
+      data: Joi.object().keys({
+        type: Joi.string(),
+        id: Joi.string()
+      })
+    })
+  })
+});
+
+const findById = Joi.object().keys({
+  data: Equipment
+});
+
+const findAll = Joi.object().keys({
+  data: Joi.array().items(Equipment)
+});
+
+const unauthorized = Joi.object().keys({
+  errors: Joi.array().items(
+    Joi.object().keys({
+      status: 401,
+      title: 'Unauthorized'
+    })
+  )
+});
+
+const notFound = Joi.object().keys({
+  errors: Joi.array().items(
+    Joi.object().keys({
+      status: 404,
+      title: 'Resource not found.'
+    })
+  )
+});
+
+const unhandledError = Joi.object().keys({
+  errors: Joi.array().items(
+    Joi.object().keys({
+      status: 500,
+      title: 'An unhandled error occurred'
+    })
+  )
+});
+
+const generateSwaggerSchemas = (entitySchema) => {
+  return {
+    'hapi-swagger': {
+      responses: {
+        200: {
+          description: 'Success',
+          schema: entitySchema
+        },
+        401: {
+          description: 'Unauthorized',
+          schema: unauthorized
+        },
+        404: {
+          description: 'Not Found',
+          schema: notFound
+        },
+        500: {
+          description: 'Unhandled Error',
+          schema: unhandledError
+        }
+      }
+    }
+  };
+};
+
+module.exports = {
+  findById: generateSwaggerSchemas(findById),
+  findAll: generateSwaggerSchemas(findAll)
+};
--- validators/equipment.js
+++ validators/equipment.js
@@ -1,11 +1,31 @@
 import Joi from 'joi';
 
-const EquipmentValidator = () => {};
-
-EquipmentValidator.prototype.validateGetParams = () => {
-  return {
-    id: Joi.string()
-  };
+const findAll = {
+  offset: Joi.number().integer()
+    .default('0')
+    .description('Paging offset'),
+  limit: Joi.number().integer()
+    .default('100')
+    .description('Paging limit')
 };
 
-module.exports = EquipmentValidator;
+const findById = {
+  id: Joi.string()
+    .required()
+    .description('Equipment ID')
+};
+
+const headers = Joi.object().keys({
+  authorization: Joi.string().required()
+}).unknown();
+
+module.exports = {
+  findAll: {
+    params: findAll,
+    headers
+  },
+  findById: {
+    params: findById,
+    headers
+  }
+};
