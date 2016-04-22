ff98c6dc7cff3bc2ddac4808ea01bbfa7199f4e4
Bruno Tavares <btavare@thoughtworks.com>
2016-04-21 21:19:33 -0300

    Cleans up the Can Variables Fetcher

--- fetcher/canVariablesFetcher.js
+++ fetcher/canVariablesFetcher.js
@@ -1,43 +1,39 @@
 import config from '../config';
-import TrackingPointFetcher from './trackingPointFetcher';
 import _ from 'lodash';
-
-const defaultRequestOptions = {
-  method: 'GET',
-  json: true
-};
-
-const createSearchUrl = (equipmentIds) => {
-  const canVariableNames = config.DEFAULT_CAN_VARIABLES.join(',');
-  const ids = equipmentIds.join(',');
-
-  return `${config.TELEMETRY_API_URL}/trackingData/search?include=trackingPoint` +
-    `&links.canVariable.name=${canVariableNames}` +
-    '&aggregations=equip_agg' +
-    '&equip_agg.property=links.trackingPoint.equipment.id' +
-    '&equip_agg.aggregations=spn_ag%2Ctp_latest_ag' +
-    '&spn_ag.property=links.canVariable.name' +
-    '&spn_ag.aggregations=spn_latest_ag' +
-    '&spn_latest_ag.type=top_hits&spn_latest_ag.sort=-links.trackingPoint.timeOfOccurrence' +
-    '&spn_latest_ag.limit=1&spn_latest_ag.include=canVariable%2CcanVariable.standardUnit' +
-    '&tp_latest_ag.type=top_hits&tp_latest_ag.sort=-links.trackingPoint.timeOfOccurrence' +
-    '&tp_latest_ag.limit=1&tp_latest_ag.fields=links.trackingPoint' +
-    '&tp_latest_ag.include=trackingPoint' +
-    `&links.trackingPoint.equipment.id=${ids}`;
-};
 
 class CanVariablesFetcher {
   constructor(httpClient) {
     this.httpClient = httpClient;
   }
 
+  urlFor(equipmentIds) {
+    const canVariableNames = config.DEFAULT_CAN_VARIABLES.join(',');
+    const ids = equipmentIds.join(',');
+
+    return `${config.TELEMETRY_API_URL}/trackingData/search?include=trackingPoint` +
+      `&links.canVariable.name=${canVariableNames}` +
+      '&aggregations=equip_agg' +
+      '&equip_agg.property=links.trackingPoint.equipment.id' +
+      '&equip_agg.aggregations=spn_ag%2Ctp_latest_ag' +
+      '&spn_ag.property=links.canVariable.name' +
+      '&spn_ag.aggregations=spn_latest_ag' +
+      '&spn_latest_ag.type=top_hits&spn_latest_ag.sort=-links.trackingPoint.timeOfOccurrence' +
+      '&spn_latest_ag.limit=1&spn_latest_ag.include=canVariable%2CcanVariable.standardUnit' +
+      '&tp_latest_ag.type=top_hits&tp_latest_ag.sort=-links.trackingPoint.timeOfOccurrence' +
+      '&tp_latest_ag.limit=1&tp_latest_ag.fields=links.trackingPoint' +
+      '&tp_latest_ag.include=trackingPoint' +
+      `&links.trackingPoint.equipment.id=${ids}`;
+  }
+
   fetchByEquipmentId(equipmentIds, authorizationBearer) {
-    let options = Object.assign({
-      uri: createSearchUrl(equipmentIds),
+    let options = {
+      uri: this.urlFor(equipmentIds),
+      method: 'GET',
+      json: true,
       headers: {
         Authorization: authorizationBearer
       }
-    }, defaultRequestOptions);
+    };
 
     let equipments = {};
 
