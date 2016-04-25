cdcd4b09feb2f176341f706fa53f33cc2f698c53
Glauco Oliveira <gl4uc0@gmail.com>
2016-04-25 13:06:42 -0300

    Fixing broken app initialization script.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import app from './app';
+import config from './config';
 import httpClient from 'request-promise';
 import CanVariablesFetcher from './fetcher/canVariablesFetcher';
 import EquipmentFetcher from './fetcher/equipmentFetcher';
-import TrackingPointFetcher from '../fetcher/trackingPointFetcher';
+import TrackingPointFetcher from './fetcher/trackingPointFetcher';
 
 const canVariablesFetcher = new CanVariablesFetcher(httpClient);
 const equipmentFetcher = new EquipmentFetcher(httpClient);
@@ -13,7 +14,11 @@ server.start((err) => {
   if (err) {
     throw err;
   }
-  console.log('Server running at:', server.info.uri);
+  if (server.info && server.info.url) {
+    console.log(`Server running at: ${server.info.url}`);
+  } else {
+    console.log(`Server running at: http://127.0.0.1:${config.PORT}`);
+  }
 });
 
 module.exports = server;
