7c577f6e35531e679caab1a9b73bd6595d46270a
Glauco Oliveira <gl4uc0@gmail.com>
2016-05-06 19:11:49 -0300

    Updating Swagger Model Names
    This commit helps us giving proper names to most (not all) Swagger
    definitions. There are still a few names that we were unable to change,
    but they might not be relevant to our documentation.

--- schemas/equipmentSchema.js
+++ schemas/equipmentSchema.js
@@ -1,80 +1,92 @@
 import Joi from 'joi';
 
-const Equipment = Joi.object().keys({
+const trackingPoint = Joi.object().keys({
+  location: Joi.object().keys({
+    coordinates: Joi.array(),
+    type: Joi.string()
+  }).label('location'),
+  status: Joi.string()
+}).label('trackingPoint');
+
+const trackingData = Joi.object().label('trackingData');
+
+const dealer = Joi.object().keys({
+  links: Joi.object().keys({
+    self: Joi.string(),
+    related: Joi.string()
+  }).label('dealerLinks'),
+  data: Joi.object().keys({
+    type: Joi.string(),
+    id: Joi.string()
+  }).label('dealerData')
+}).label('dealer');
+
+const model = Joi.object().keys({
+  links: Joi.object().keys({
+    self: Joi.string(),
+    related: Joi.string()
+  }).label('equipmentModelLinks'),
+  data: Joi.object().keys({
+    type: Joi.string(),
+    id: Joi.string()
+  }).label('equipmentModelData')
+}).label('equipmentModel');
+
+const equipmentAttributes = Joi.object().keys({
+  description: Joi.string(),
+  serviceLevel: Joi.number().integer(),
+  identificationNumber: Joi.string(),
+  manufacturingDate: Joi.date(),
+  trackingPoint,
+  trackingData
+}).label('equipmentAttributes');
+
+const equipmentRelationships = Joi.object().keys({
+  dealer,
+  model
+}).label('equipmentRelationships');
+
+const equipment = Joi.object().keys({
   type: Joi.string(),
   id: Joi.string(),
-  attributes: Joi.object().keys({
-    description: Joi.string(),
-    serviceLevel: Joi.number().integer(),
-    identificationNumber: Joi.string(),
-    manufacturingDate: Joi.date(),
-    trackingPoint: Joi.object().keys({
-      location: Joi.object().keys({
-        coordinates: Joi.array(),
-        type: Joi.string()
-      }),
-      status: Joi.string()
-    }),
-    trackingData: Joi.object()
-  }),
-  relationships: Joi.object().keys({
-    dealer: Joi.object().keys({
-      links: Joi.object().keys({
-        self: Joi.string(),
-        related: Joi.string()
-      }),
-      data: Joi.object().keys({
-        type: Joi.string(),
-        id: Joi.string()
-      })
-    }),
-    model: Joi.object().keys({
-      links: Joi.object().keys({
-        self: Joi.string(),
-        related: Joi.string()
-      }),
-      data: Joi.object().keys({
-        type: Joi.string(),
-        id: Joi.string()
-      })
-    })
-  })
-});
+  attributes: equipmentAttributes,
+  relationships: equipmentRelationships
+}).label('equipment');
 
 const findById = Joi.object().keys({
-  data: Equipment
-});
+  data: equipment
+}).label('anEquipmentData');
 
 const findAll = Joi.object().keys({
-  data: Joi.array().items(Equipment)
-});
+  data: Joi.array().items(equipment)
+}).label('equipmentData');
 
 const unauthorized = Joi.object().keys({
   errors: Joi.array().items(
     Joi.object().keys({
       status: 401,
       title: 'Unauthorized'
-    })
-  )
-});
+    }).label('unauthorizedError')
+  ).label('unauthorizedErrors')
+}).label('401');
 
 const notFound = Joi.object().keys({
   errors: Joi.array().items(
     Joi.object().keys({
       status: 404,
       title: 'Resource not found.'
-    })
-  )
-});
+    }).label('resourceNotFound')
+  ).label('resourcesNotFound')
+}).label('404');
 
 const unhandledError = Joi.object().keys({
   errors: Joi.array().items(
     Joi.object().keys({
       status: 500,
       title: 'An unhandled error occurred'
-    })
-  )
-});
+    }).label('unhandledError')
+  ).label('unhandledErrors')
+}).label('500');
 
 const generateSwaggerSchemas = (entitySchema) => {
   return {
