cadc3fe61ea1077339527fc75be08e87e654284a
Glauco Oliveira <gl4uc0@gmail.com>
2016-05-20 14:26:32 -0300

    Including missing top-level elements.
    JSON API payloads should contain at least one of the following top-level
    members:
    
    data: the document’s 'primary data'
    errors: an array of error objects
    meta: a meta object that contains non-standard meta-information.
    
    This commit includes an empty `data` element as well as a `meta` element
    whose content will reflect our existing endpoint documentation.

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -5,6 +5,13 @@ class RootController {
 
   findAll(request, reply) {
     return reply({
+      meta: {
+        description: 'The Equipment API is a façade whose goal is to provide an ' +
+          'easier way of retrieving equipment related structured information. ' +
+          'The equipment information are retrieved from the Telemetry API and ' +
+          'contains the same values, with the structure modeled after the ' +
+          'equipment concept.'
+      },
       links: {
         self: {
           href: '/',
--- test/controllers/root_controller_spec.js
+++ test/controllers/root_controller_spec.js
@@ -38,6 +38,13 @@ describe('RootController', () => {
       };
 
       const expectedResponse = {
+        meta: {
+          description: 'The Equipment API is a façade whose goal is to provide an ' +
+            'easier way of retrieving equipment related structured information. ' +
+            'The equipment information are retrieved from the Telemetry API and ' +
+            'contains the same values, with the structure modeled after the ' +
+            'equipment concept.'
+        },
         links: {
           self: {
             href: '/',
