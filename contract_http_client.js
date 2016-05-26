80b9854a7c855a9f37b744946d48de40a7338ee2
Waldemar Neto <waldemarnt@gmail.com>
2016-05-26 16:14:31 -0300

    Changes argument name to be more clear

--- contract_http_client.js
+++ contract_http_client.js
@@ -1,8 +1,8 @@
 import requestPromise from 'request-promise';
 import helper from './contract_helper';
 
-const contractHttpRequest = (request, cb) => {
-  const contractRequest = request;
+const contractHttpRequest = (contractRequest, cb) => {
+  const contractRequest = contractRequest;
   helper.requestOAuthAccessToken()
   .then((accessToken) => {
     contractRequest.headers = {
