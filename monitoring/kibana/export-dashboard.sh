ba34628912ff2df3416b83568c96ab2cb1248bf0
Lucas <llago@thoughtworks.com>
2016-06-23 18:41:26 -0300

    fix indentation

--- monitoring/kibana/export-dashboard.sh
+++ monitoring/kibana/export-dashboard.sh
@@ -3,8 +3,8 @@ rm ./monitoring/kibana/dashboard.json
 elasticdump --input=http://192.168.99.100:9200/.kibana --output=./monitoring/kibana/dashboard.json --searchBody='
 {
   "query": {
-    "wildcard": 
-      { "title": {"value": "default*"} }
+    "wildcard": {
+      "title": {"value": "default*"}
+    }
   }
-        
-}' 
+}'
